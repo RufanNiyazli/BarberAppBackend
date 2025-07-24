40e48fccc816507bfb760ef9f5bb19bed45cf811
RufanNiyazli <developerx73@gmail.com>
2025-07-24 20:41:33 +0400

    start

---
+++ src/test/java/com/project/barberreservation/BarberReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.barberreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
